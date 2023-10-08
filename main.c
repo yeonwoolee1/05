cc4a09e8794dadd45ce13003e5ac2214db9a7f2f
yeonwoolee1 <144992849+yeonwoolee1@users.noreply.github.com>
2023-10-08 23:00:59 +0900

    실습6

--- main.c
+++ main.c
@@ -4,17 +4,25 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int sum=0;
+	int answer=45;
 	int num;
-	int i;
+	int trial=0;
 	
-	printf("input a number:");
-	scanf("%d",&num);
-	
-	for(i=0; i<=num; i++)
+	do
 	{
-		sum=sum+i;
+		printf("guess a number:");
+		scanf("%d",&num);
+		
+		if (num<answer)
+			printf("high!\n");
+		else if(num>answer)
+			printf("down!\n");
+			
+		trial=trial+1;
 	}
-	printf("the result is %i\n",sum);
+	while(num!=answer);
+	
+	printf("congratulation! trials:%d\n",trial);
+	
 	return 0;
 }
