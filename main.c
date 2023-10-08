38fdc1f3ff43f713605a7a472705c45b8e207ae7
yeonwoolee1 <144992849+yeonwoolee1@users.noreply.github.com>
2023-10-08 21:50:33 +0900

    실습3

--- main.c
+++ main.c
@@ -4,13 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int a;
-	printf("number:");
-	scanf("%i",&a);
-	if (a>=0)
-		printf("absolute value: %i\n",a);
-	else
-		printf("absolute value: %i\n",-a); 
+	char c;
+	int num=0;
+	printf("input a string:");
+	while ( (c=getchar() )!='\n')
+	{
+		if (c>='0'&& c<='9')
+			num=num+1;
+	}
 	
+	printf("the number if digits is %i\n",num);
 	return 0;
 }
